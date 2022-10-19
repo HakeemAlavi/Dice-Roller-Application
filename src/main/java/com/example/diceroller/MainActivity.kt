caad917b5d7ee3aa417c4a0537edacd50a081b86
Hakeem Alavi <103939681+HakeemAlavi@users.noreply.github.com>
2022-10-19 10:19:03 +0300

    Updated src file
    I added the Hello Toast button and function

--- src/main/java/com/example/diceroller/MainActivity.kt
+++ src/main/java/com/example/diceroller/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.diceroller
 
 import android.os.Bundle
+import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -30,10 +32,9 @@ class MainActivity : AppCompatActivity() {
             return (1..numSides).random()
         }
     }
+
+    fun showToast(view: View) {
+        Toast.makeText(this,"Hello Toast!", Toast.LENGTH_SHORT).show()
+    }
 }
 
-//val toast = Toast.makeText(this, "Dice Rolled!", Toast.LENGTH_SHORT)
-//toast.show()
-
-//val resultTextView: TextView = findViewById(R.id.textView)
-//resultTextView.text = "6"
